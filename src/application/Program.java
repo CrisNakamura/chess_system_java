86dcc83f6743aae45e40f6c4df651e61e937bbdd
Cris Nakamura <natsuminakamura98@gmail.com>
2021-01-11 11:53:50 -0300

    Switching player each turn

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,27 +9,27 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 
 public class Program {
-
+    
     public static void main(String[] args) {
-
+        
         Scanner scan = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-
+        
         while (true) {
             try {
                 UI.clearScreen();
-                UI.printBoard(chessMatch.getPieces());
+                UI.printMatch(chessMatch);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(scan);
-
+                
                 boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(scan);
-
+                
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
