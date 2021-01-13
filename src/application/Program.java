2e6888d49fd62c8beaee11681939ff6c26970fbc
Cris Nakamura <natsuminakamura98@gmail.com>
2021-01-13 17:34:20 -0300

    Checkmate Logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,34 +11,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Program {
-
+    
     public static void main(String[] args) {
-
+        
         Scanner scan = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
-
-        while (true) {
+        
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(scan);
-
+                
                 boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(scan);
-
+                
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-
+                
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
-
+                
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
                 scan.nextLine();
@@ -47,5 +47,7 @@ public class Program {
                 scan.nextLine();
             }
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
