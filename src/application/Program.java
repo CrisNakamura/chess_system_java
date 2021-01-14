6f126c5e1075f7a7ee63daed73dcc799a1c98d00
Cris Nakamura <natsuminakamura98@gmail.com>
2021-01-13 23:03:03 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,13 +11,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Program {
-    
+
     public static void main(String[] args) {
-        
+
         Scanner scan = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
-        
+
         while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
@@ -25,20 +25,24 @@ public class Program {
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(scan);
-                
+
                 boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(scan);
-                
+
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-                
+
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
-                
+                if (chessMatch.getPromoted() != null) {
+                    System.out.print("Enter piece for promotion (B/N/R/Q):");
+                    String type = scan.nextLine();
+                    chessMatch.replacePromotedPiece(type);
+                }
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
                 scan.nextLine();
